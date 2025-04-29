78987a457a36d3d337cf52cabc0aed9454e3857d
danichagas <chagasdaniel788@gmail.com>
2025-04-29 13:54:46 -0300

    feat: getting the selected currency

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
+const form = document.querySelector('form')
 const amount = document.getElementById('amount')
+const currency = document.getElementById('currency')
 
 amount.addEventListener('input', () => {
     const hasCaractersRegex = /\D+/g
 
     amount.value = amount.value.replace(hasCaractersRegex, "")
 })
+
+form.onsubmit = (event) => {
+    event.preventDefault()
+
+    console.log(currency.value)
+}
