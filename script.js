31619eecb22f1376f9d172a6282de3e478cea661
danichagas <chagasdaniel788@gmail.com>
2025-04-29 14:33:20 -0300

    feat: identifying the selected currency

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const USD = 5.64
+const EUR = 6.43
+const GBP = 7.56
+
 const form = document.querySelector('form')
 const amount = document.getElementById('amount')
 const currency = document.getElementById('currency')
@@ -11,5 +15,25 @@ amount.addEventListener('input', () => {
 form.onsubmit = (event) => {
     event.preventDefault()
 
-    console.log(currency.value)
+    switch (currency.value) {
+        case 'USD': 
+            convertCurrency(amount.value, USD, 'US$')
+            break
+        case 'EUR':
+            convertCurrency(amount.value, EUR, '€')
+            break
+        case 'GBP':
+            convertCurrency(amount.value, GBP, '£')
+            break
+    }
+}
+
+/**
+ * 
+ * @param {Number} amount quantia que o usúario vai desejar converter
+ * @param {Number} price preço da moeda selecionada
+ * @param {String} symbol símbolo da moeda selecionada
+ */
+function convertCurrency(amount, price, symbol) {
+    console.log(amount, price, symbol)
 }
